1c07d804a45e52d9d4c5e522e2db85e19e220c1e
marion419 <daynvi@gmail.com>
2024-12-04 18:19:58 +0900

    [add]appBar 검색 영역 추가

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            decoration: InputDecoration(
+              hintText: '읍/면/동으로 검색하세요',
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
