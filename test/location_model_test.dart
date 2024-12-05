0daaea4c9a0a70dd17c498f63891588a13da77cf
marion419 <daynvi@gmail.com>
2024-12-05 20:59:10 +0900

    [test] 모델 테스트 작성

---
+++ test/location_model_test.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:flutter_location_app/data/model/location.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('location model test', () {
+    String dummyData = """
+{
+  "title": "title",
+  "category": "category",
+  "roadAddress": "roadAddress"
+}
+""";
+
+    Map<String, dynamic> map = jsonDecode(dummyData);
+    Location location = Location.fromJson(map);
+    expect(location.title, 'title');
+  });
+}
