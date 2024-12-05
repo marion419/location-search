3bdf8866a1a1b5304764e1441335170b34d0e7f9
marion419 <daynvi@gmail.com>
2024-12-05 20:59:39 +0900

    [test] 레포지토리 테스트에 print문 추가

--- test/location_repository_test.dart
+++ test/location_repository_test.dart
@@ -2,9 +2,12 @@ import 'package:flutter_location_app/data/repository/location_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  final locationRepo = LocationRepository();
   test('locationTest', () async {
+    LocationRepository locationRepo = LocationRepository();
     final result = await locationRepo.findByLocation('삼성동');
     expect(result.isEmpty, false);
+    for (var loca in result) {
+      print(loca.toJson());
+    }
   });
 }
