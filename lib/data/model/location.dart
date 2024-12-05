9c72b4b4fbbf9e284c23fefd865b5359f7b1fd9b
marion419 <daynvi@gmail.com>
2024-12-05 18:14:20 +0900

    [add] Location 모델 파일 생성

---
+++ lib/data/model/location.dart
@@ -0,0 +1,11 @@
+class Location {
+  String title;
+  String category;
+  String address;
+
+  Location({
+    required this.title,
+    required this.category,
+    required this.address,
+  });
+}
