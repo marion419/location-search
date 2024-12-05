5d7f3ebcda51befcb486944e96246e313534b9a4
marion419 <daynvi@gmail.com>
2024-12-05 20:58:44 +0900

    [fix] LocationRepository에 모델 적용

--- lib/data/repository/location_repository.dart
+++ lib/data/repository/location_repository.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:flutter_location_app/data/model/location.dart';
 
 class LocationRepository {
   final Dio _client = Dio(BaseOptions(
     validateStatus: (status) => true,
   ));
 
-  Future<List<String>> findByLocation(String query) async {
+  Future<List<Location>> findByLocation(String query) async {
     final response = await _client.get(
       'https://openapi.naver.com/v1/search/local.json',
       queryParameters: {
@@ -19,10 +22,11 @@ class LocationRepository {
     );
     try {
       // 응답 코드가 200인 경우 받은 데이터로 list 생성
+      // 데이터 경로 : items >> title/category/
       if (response.statusCode == 200) {
         final items = response.data['items'];
-        final iterable = List.from(items).map((item) {
-          return '${item['title']}';
+        final iterable = List.from(items).map((e) {
+          return Location.fromJson(e);
         });
         return iterable.toList();
       }
