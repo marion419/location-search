130f52d977ee6b1aeb66302c0c2a8d182e4d3f4d
marion419 <daynvi@gmail.com>
2024-12-06 16:12:10 +0900

    [delete] unused import 삭제

--- lib/data/repository/location_repository.dart
+++ lib/data/repository/location_repository.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:flutter_location_app/data/model/location.dart';
 
--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_location_app/ui/pages/home/home_view_model.dart';
 import 'package:flutter_location_app/ui/pages/home/widgets/home_search_bar.dart';
 import 'package:flutter_location_app/ui/pages/home/widgets/location_list_view.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
