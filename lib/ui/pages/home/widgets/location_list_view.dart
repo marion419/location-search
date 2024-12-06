e9bd77fc907833218e8eb20fe56a8c8fb83dca45
marion419 <daynvi@gmail.com>
2024-12-06 16:43:03 +0900

    [add] listview에서 detailpage로 네비게이터 추가

--- lib/ui/pages/home/widgets/location_list_view.dart
+++ lib/ui/pages/home/widgets/location_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_location_app/ui/pages/detail/detail_page.dart';
 import 'package:flutter_location_app/ui/pages/home/home_view_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -25,34 +26,44 @@ class LocationListView extends StatelessWidget {
               } else {
                 return Padding(
                   padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      //border: Border.all(color: Colors.grey[700]!),
-                      color: Colors.grey[200],
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            locations[index].title,
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                DetailPage(link: locations[index].link),
+                          ));
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        //border: Border.all(color: Colors.grey[700]!),
+                        color: Colors.grey[200],
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              locations[index].title,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            locations[index].category,
-                            style: TextStyle(
-                              fontSize: 16,
+                            Text(
+                              locations[index].category,
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
                             ),
-                          ),
-                          Text(
-                            locations[index].roadAddress,
-                          ),
-                        ],
+                            Text(
+                              locations[index].roadAddress,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
