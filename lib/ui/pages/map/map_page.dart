4318e299a1be7788d2694657158e063c5f948b72
marion419 <daynvi@gmail.com>
2024-12-09 20:03:40 +0900

    [add]카카오맵 추가가

--- lib/ui/pages/map/map_page.dart
+++ lib/ui/pages/map/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kakaomap_webview/kakaomap_webview.dart';
 
 class MapPage extends StatelessWidget {
   String address;
@@ -8,7 +9,27 @@ class MapPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Google Map'),
+        title: Text(address),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 100),
+            KakaoMapView(
+                width: 400,
+                height: 400,
+                kakaoMapKey: '08eebe69029ef27e6209ec2d97b79d29',
+                lat: 33.450701,
+                lng: 126.570667,
+                showMapTypeControl: true,
+                showZoomControl: true,
+                markerImageURL:
+                    'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
+                onTapMarker: (message) {
+                  //event callback when the marker is tapped
+                }),
+          ],
+        ),
       ),
     );
   }
