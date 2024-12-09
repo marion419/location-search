182f9ca963f823bd3d5842032c9500725f58bfa2
marion419 <daynvi@gmail.com>
2024-12-09 17:19:36 +0900

    [fix] findDistrict 반환타입 List<String>으로 변경

--- lib/data/repository/vworld_repository.dart
+++ lib/data/repository/vworld_repository.dart
@@ -1,31 +1,38 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_location_app/data/model/location.dart';
 
 class VworldRepository {
   final Dio _client = Dio(BaseOptions(
     validateStatus: (status) => true,
   ));
 
-  Future<String> findDistrict(double lat, double lng) async {
-    final response = await _client.get(
-      'https://api.vworld.kr/req/data',
-      queryParameters: {
-        'request': 'GetFeature',
-        'key': 'DB7D3ACC-ABA6-357F-B0DE-D5C915CE71B5',
-        'data': 'LT_C_ADEMD_INFO',
-        'geomfilter': 'POINT($lng $lat)',
-        'geometry': 'false',
-      },
-    );
+  Future<List<String>> findDistrict(double lat, double lng) async {
     try {
+      final response = await _client.get(
+        'https://api.vworld.kr/req/data',
+        queryParameters: {
+          'request': 'GetFeature',
+          'key': 'DB7D3ACC-ABA6-357F-B0DE-D5C915CE71B5',
+          'data': 'LT_C_ADEMD_INFO',
+          'geomFilter': 'POINT($lng $lat)',
+          'geometry': false,
+        },
+      );
+
       if (response.statusCode == 200 &&
           response.data['response']['status'] == 'OK') {
-        return response.data['response']['result']['features']['properties']
-            ['emd_kor_nm'];
+        final features = response.data['response']['result']
+            ['featureCollection']['features'];
+        final featureList = List.from(features);
+        final iterable = featureList.map((feat) {
+          return '${feat['properties']['emd_kor_nm']}';
+        });
+        return iterable.toList();
       }
-      return '';
+      return [];
     } catch (e) {
       print(e);
-      return '';
+      return [];
     }
   }
 }
