e415767a0d4affbb5a8f9b5620b4d6ff6230c43a
marion419 <daynvi@gmail.com>
2024-12-09 21:05:56 +0900

    [add] 지도 마커 누르면 DetailPage로 이동하는 기능 추가

--- lib/ui/pages/map/map_page.dart
+++ lib/ui/pages/map/map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_location_app/data/model/location.dart';
+import 'package:flutter_location_app/ui/pages/detail/detail_page.dart';
 import 'package:flutter_location_app/ui/pages/map/widgets/information_box.dart';
 import 'package:kakaomap_webview/kakaomap_webview.dart';
 
@@ -27,6 +28,16 @@ class MapPage extends StatelessWidget {
                 lat: double.parse(location.latitude) / 10000000,
                 lng: double.parse(location.longitude) / 10000000,
                 showZoomControl: true,
+                onTapMarker: (p0) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return DetailPage(location);
+                      },
+                    ),
+                  );
+                },
               ),
               SizedBox(height: 50),
               Column(
